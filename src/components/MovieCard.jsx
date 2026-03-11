c3063359db91266b4c0d6be7651cb9f49a3f082d
Joseph <joengash650@gmail.com>
2026-03-11 23:30:02 +0300

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,15 @@
-function MovieCard({ title, image, rating }) {
+function MovieCard({
+  id,
+  title,
+  image,
+  rating,
+  isFavorite,
+  onToggleFavorite,
+  onSelectMovie,
+}) {
   return (
     <div
+      onClick={() => onSelectMovie({ id, title, image, rating, isFavorite })}
       style={{
         border: "1px solid #ccc",
         borderRadius: "8px",
@@ -9,6 +18,7 @@ function MovieCard({ title, image, rating }) {
         width: "200px",
         textAlign: "center",
         boxShadow: "2px 2px 8px rgba(0,0,0,0.1)",
+        cursor: "pointer",
       }}
     >
       <img
@@ -18,6 +28,23 @@ function MovieCard({ title, image, rating }) {
       />
       <h3>{title}</h3>
       <p>⭐ {rating}</p>
+
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          onToggleFavorite(id);
+        }}
+        style={{
+          padding: "5px 10px",
+          backgroundColor: isFavorite ? "red" : "gray",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        {isFavorite ? "❤️ Favorited" : "🤍 Add to Favorites"}
+      </button>
     </div>
   );
 }
